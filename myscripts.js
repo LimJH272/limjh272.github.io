eccda39b267a457990f6b7f95ccd8209551ba593
LimJH272 <limjiehan02@gmail.com>
2023-05-25 14:13:29 +0800

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -8,7 +8,7 @@ const age = setInterval(function() {
     const current_array = current_date.toString().split(" ");
 
     if (birth_array[1] == current_array[1] && birth_array[2] == current_array[2]) {
-        document.querySelector("#birthday").classList.add(".text-primary");
+        document.querySelector("#birthday").classList.add("text-primary");
         document.querySelector("#age").innerHTML = parseInt(current_array[3]) - parseInt(birth_array[3]);
     } else {
         document.querySelector("#birthday").classList.remove("text-primary");
