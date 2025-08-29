eb88b0e9c13d946ec47e32f3dc9dc07292e597bf
LimJH272 <limjiehan02@gmail.com>
2025-08-29 23:16:09 +0800

    Run JS script only if needed

--- myscripts.js
+++ myscripts.js
@@ -7,11 +7,13 @@ const currDate = new Date();
 const bdayElement = document.querySelector("#birthday");
 const ageElement = document.querySelector("#age");
 
-// Calculate age
-const thisYearBirthDate = new Date(currDate.getFullYear(), birthDate.getMonth(), birthDate.getDate());
-ageElement.innerHTML = currDate.getFullYear() - birthDate.getFullYear() - ((currDate.getTime() - thisYearBirthDate.getTime() >= 0) ? 0 : 1);
+if (bdayElement !== null && ageElement !== null) {
+    // Calculate age
+    const thisYearBirthDate = new Date(currDate.getFullYear(), birthDate.getMonth(), birthDate.getDate());
+    ageElement.innerHTML = currDate.getFullYear() - birthDate.getFullYear() - ((currDate.getTime() - thisYearBirthDate.getTime() >= 0) ? 0 : 1);
 
-// Highlight if today is my birthday
-if ((birthDate.getDate() == currDate.getDate()) && (birthDate.getMonth() == currDate.getMonth())) {
-        bdayElement.classList.add("text-primary");
+    // Highlight if today is my birthday
+    if ((birthDate.getDate() == currDate.getDate()) && (birthDate.getMonth() == currDate.getMonth())) {
+            bdayElement.classList.add("text-primary");
+    }
 }
